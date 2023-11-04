e8997733070cf81f3bcb049bdd26e2aa927906e4
erinbedri <erinbedri@gmail.com>
2023-11-04 18:13:17 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <h1>Chat App</h1>
+            <main>
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>} />
+
+                    <Route path="/login" element={<h1>Login</h1>} />
+
+                    <Route path="/register" element={<h1>Register</h1>} />
+
+                    <Route path="/logout" element={<h1>logout</h1>} />
+
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+            </main>
         </div>
     );
 }
